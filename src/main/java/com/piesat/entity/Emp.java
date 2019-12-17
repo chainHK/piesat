3d23eb303d8eb55e968d3932548f3121399a3d94
xikai <huikai@piesat.cn>
2019-12-17 09:13:46 +0800

    redis+sentinel配置

--- src/main/java/com/piesat/entity/Emp.java
+++ src/main/java/com/piesat/entity/Emp.java
@@ -9,8 +9,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
@@ -24,6 +26,8 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Emp implements Serializable{
 
     private static final long serialVersionUID = 1L;
