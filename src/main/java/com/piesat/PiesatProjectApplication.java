b6370095ea9697ea8e331e2de3b72cd437b93138
xikai <huikai@piesat.cn>
2019-12-18 10:14:44 +0800

    修改了项目名字

--- src/main/java/com/piesat/PiesatProjectApplication.java
+++ src/main/java/com/piesat/PiesatProjectApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.piesat.mapper")
 @SpringBootApplication
-public class MavenPluginBuildDockerApplication {
+public class PiesatProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MavenPluginBuildDockerApplication.class, args);
+		SpringApplication.run(PiesatProjectApplication.class, args);
 	}
 
 }
